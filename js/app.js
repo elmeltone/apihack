36e4f576d6879390957a550a1c103b8db3c801d2
MICHAEL J MELTON <meltonm83@gmail.com>
2016-03-21 12:44:47 -0700

    first var

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 $(function() {
 	
+	var showImage = function(image) {
+		var result = $('.template .general').clone();
+		
+		var imageElemLink = result.find('.image a');
+		if image.links.resource != null(imageElem.attr('href', image.links.resource);)
+			else imageElem.attr('href', image.links.item);
+		var imageElemPic = result.find('.image img');
+		imageElemPic.attr("src", image.image.thumb);
+		
+		var imgName = result.find('')
+	};
+	
+	
+	
+	
+	
 	$('#general').on('click', function() {
 		
 	})
