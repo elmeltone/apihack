b1b41b9519e07ed70d32d5cb226abdf25fbed1d6
MICHAEL J MELTON <meltonm83@gmail.com>
2016-03-21 13:09:00 -0700

    showImage

--- js/app.js
+++ js/app.js
@@ -4,16 +4,29 @@ $(function() {
 		var result = $('.template .general').clone();
 		
 		var imageElemLink = result.find('.image a');
-		if image.links.resource != null(imageElem.attr('href', image.links.resource);)
-			else imageElem.attr('href', image.links.item);
+		if (image.links.resource != null)
+			imageElemLink.attr('href', image.links.resource);
+		else {imageElemLink.attr('href', image.links.item)};
 		var imageElemPic = result.find('.image img');
 		imageElemPic.attr("src", image.image.thumb);
 		
-		var imgName = result.find('')
+		var imgCreator = result.find('.image-creator a');
+		if (image.links.resource != null)
+			imgCreator.attr('href', image.links.resource);
+		else {imgCreator.attr('href', image.links.item)};
+		imgCreator.text(image.creator);
+		
+		var imgCallNum = results.find('.image-callnumber');
+		imgCallNum.text(image.call_number);
+		
+		var imgNotes = results.find('.image-notes');
+		if (image.title != null)
+			imgNotes.text(image.title);
+		else {imgNotes.text("No title")};
+		
+		return result;
+		
 	};
-	
-	
-	
 	
 	
 	$('#general').on('click', function() {
