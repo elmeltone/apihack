b44581ee9c4b90d0ee7e4664cf607a8edd6c4bda
MICHAEL J MELTON <meltonm83@gmail.com>
2016-03-23 13:05:34 -0700

    returns bullets

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 	var showImage = function(target) {
-		var result = $('.template .general').clone();
+		var result = $('#general-image').clone();
 		
 		var imageElemLink = result.find('.image a');
 		if (target.links.resource != null)
@@ -9,7 +9,7 @@
 		var imageElemPic = result.find('.image img');
 		imageElemPic.attr("src", target.image.thumb);
 
-		var imgCreator = result.find('.image-creator a');
+		/*var imgCreator = result.find('.image-creator a');
 		if (target.links.resource != null)
 			imgCreator.attr('href', target.links.resource);
 		else {imgCreator.attr('href', target.links.item)};
@@ -21,7 +21,7 @@
 		var imgNotes = result.find('.image-notes');
 		if (target.title != null)
 			imgNotes.text(target.title);
-		else {imgNotes.text("No title")};
+		else {imgNotes.text("No title")};*/
 
 		return result;
 		
