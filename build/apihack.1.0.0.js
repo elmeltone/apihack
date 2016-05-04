2a8a01c29adbe720ee6afe44c85e9c592d0f9be3
MICHAEL J MELTON <meltonm83@gmail.com>
2016-05-04 12:45:55 -0700

    add es6 with babel and add uglify

--- build/apihack.1.0.0.js
+++ build/apihack.1.0.0.js
@@ -44,45 +44,44 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	var $ = __webpack_require__(1);
 	var showImage = __webpack_require__(2);
 	var showSearchResults = __webpack_require__(3);
 	var showError = __webpack_require__(4);
 	var showNextPage = __webpack_require__(5);
 	
-	var getImages = function(query, pageNum) {
-			
+	var getImages = function getImages(query, pageNum) {
+	
 		var url = 'http://loc.gov/pictures/search/?q=' + query;
-				
+	
 		$.ajax({
-		    type: 'search',
-		    url: url,
-		    dataType:'jsonp',
-		    data:{
-		        fo:'json',
-				sp: pageNum,
-		    },
-		})
-		.done(function(result){	
+			type: 'search',
+			url: url,
+			dataType: 'jsonp',
+			data: {
+				fo: 'json',
+				sp: pageNum
+			}
+		}).done(function (result) {
 			var searchResults = showSearchResults(query, result.search.hits);
 			$('.results').html(searchResults);
-			$.each(result.results, function(i, item) {
+			$.each(result.results, function (i, item) {
 				var printImage = showImage(item);
 				$(printImage).appendTo('.results');
 			});
-		})		
-		.done(function(result){
-			var nextPage = showNextPage(query/*, result.pages.next*/);
-			var endPage = '<p id="endPage">End results</p>'
-			if (result.pages.next != ((null) || (undefined)))
-				$(nextPage).appendTo('.results');
-			else {$(endPage).appendTo('.results')};
-			});
-			
+		}).done(function (result) {
+			var nextPage = showNextPage(query /*, result.pages.next*/);
+			var endPage = '<p id="endPage">End results</p>';
+			if (result.pages.next != (null || undefined)) $(nextPage).appendTo('.results');else {
+				$(endPage).appendTo('.results');
+			};
+		});
 	};
-		
-	$(function() {	
-		$('.image-getter').on('submit', function(e) {
+	
+	$(function () {
+		$('.image-getter').on('submit', function (e) {
 			e.preventDefault();
 			$('.results').html('');
 			var pageCounter = 1;
@@ -91,7 +90,7 @@
 		});
 		$('.count').on('click', function (e) {
 			e.preventDefault();
-			var currentPage = pageCounter+1;
+			var currentPage = pageCounter + 1;
 			getImages(query, currentPage);
 		});
 	});
@@ -9948,41 +9947,42 @@
 /* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	var $ = __webpack_require__(1);
 	var showSearchResults = __webpack_require__(3);
 	var showError = __webpack_require__(4);
 	var showNextPage = __webpack_require__(5);
 	
-	var showImage = function(target) {
+	var showImage = function showImage(target) {
 		var result = $('#general-image').clone();
-		
+	
 		var imgLink1 = "http:" + target.links.resource;
 		var imgLink2 = "http:" + target.links.item;
 		var imgThumb = "http:" + target.image.thumb;
-		
+	
 		var imageElemLink = result.find('.image a');
-		if (imgLink1 != null)
-			imageElemLink.attr('href', imgLink1);
-		else {imageElemLink.attr('href', imgLink2)};
+		if (imgLink1 != null) imageElemLink.attr('href', imgLink1);else {
+			imageElemLink.attr('href', imgLink2);
+		};
 		var imageElemPic = result.find('.image img');
 		imageElemPic.attr("src", imgThumb);
 	
 		var imgCreator = result.find('.image-creator a');
-		if (imgLink1 != null)
-			imgCreator.attr('href', imgLink1);
-		else {imgCreator.attr('href', imgLink2)};
+		if (imgLink1 != null) imgCreator.attr('href', imgLink1);else {
+			imgCreator.attr('href', imgLink2);
+		};
 		imgCreator.text(target.creator);
 	
 		var imgCallNum = result.find('.image-callnumber');
 		imgCallNum.text(target.call_number);
 	
 		var imgNotes = result.find('.image-notes');
-		if (target.title != null)
-			imgNotes.text(target.title);
-		else {imgNotes.text("No title")};
+		if (target.title != null) imgNotes.text(target.title);else {
+			imgNotes.text("No title");
+		};
 	
 		return result;
-		
 	};
 	
 	module.exports = showImage;
@@ -9991,28 +9991,32 @@
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	var $ = __webpack_require__(1);
 	var showImage = __webpack_require__(2);
 	var showError = __webpack_require__(4);
 	var showNextPage = __webpack_require__(5);
-		
-	var showSearchResults = function(query, resultNum) {
+	
+	var showSearchResults = function showSearchResults(query, resultNum) {
 		var results = '<p class="count"><strong>' + resultNum + ' results...</strong></p>';
 		return results;
 	};
-		
+	
 	module.exports = showSearchResults;
 
 /***/ },
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	var $ = __webpack_require__(1);
 	var showImage = __webpack_require__(2);
 	var showSearchResults = __webpack_require__(3);
 	var showNextPage = __webpack_require__(5);
 	
-	var showError = function(error){
+	var showError = function showError(error) {
 		var errorElem = $('.template .error').clone();
 		var errorText = '<p>' + error + '</p>';
 		errorElem.append(errorText);
@@ -10024,12 +10028,14 @@
 /* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+	
 	var $ = __webpack_require__(1);
 	var showImage = __webpack_require__(2);
 	var showSearchResults = __webpack_require__(3);
 	var showError = __webpack_require__(4);
 	
-	var showNextPage = function(query) {
+	var showNextPage = function showNextPage(query) {
 		var page = '<a><p class="count">Next Page</p></a>';
 		return page;
 	};
